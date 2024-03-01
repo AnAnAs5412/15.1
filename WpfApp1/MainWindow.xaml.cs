82dfd4484198b48e2e2bc07699b8d344a61c0030
vafina.ar2094 <vafina.ar2094@COM209-L4>
2024-03-01 09:06:43 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Click(object sender, MouseButtonEventArgs e)
+        {
+            Button button = new Button();
+            button.Content = "Кнопка";
+            button.Width = 100;
+            button.Height = 50;
+            Canvas.SetLeft(button, e.GetPosition(canvas).X);            
+            Canvas.SetTop(button, e.GetPosition(canvas).Y);
+            canvas.Children.Add(button);
+        }
+    }
+}
